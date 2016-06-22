ae4c1d12282e6dfc72412e8d1858b82527d5f142
Sven <sk@qyotta.de>
2016-06-22 21:54:18 +0200

    add cytoscape plugins

--- generator/src/main/java/de/axonvisualizer/generator/AxonvisualizerApplication.java
+++ generator/src/main/java/de/axonvisualizer/generator/AxonvisualizerApplication.java
@@ -1,7 +1,7 @@
 package de.axonvisualizer.generator;
 
 import de.axonvisualizer.generator.generator.Generator;
-import de.axonvisualizer.generator.json.provider.simple.SimpleDataProvider;
+import de.axonvisualizer.generator.json.provider.cytoscape.CytoscapeDataProvider;
 import de.axonvisualizer.generator.json.writer.gson.GsonWriter;
 
 public class AxonvisualizerApplication {
@@ -10,7 +10,7 @@ public class AxonvisualizerApplication {
    public static final String OUTPUT_PATH = "/Development/axonvisualizer/webapp/app/data/output.json";
 
    public static void main(String[] args) {
-      final Generator generator = new Generator(new SimpleDataProvider(), new GsonWriter());
+      final Generator generator = new Generator(new CytoscapeDataProvider(), new GsonWriter());
       generator.generateFile(INPUT_ROOT, OUTPUT_PATH);
    }
 }
--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -1,7 +1,7 @@
 $.ajax("data/output.json")
     .done(function (data) {
-        window.cy = cytoscape({
-            container: document.getElementById('diagram'),
+        var cy = cytoscape({
+            container: $('#diagram'),
             style: [
                 {
                     selector: 'node',
@@ -47,9 +47,83 @@ $.ajax("data/output.json")
             elements: data
         });
 
-        window.cy.layout({
-            name: 'grid',
-            avoidOverlap: true,
-            avoidOverlapPadding: 10,
+        // the default values of each option are outlined below:
+        var defaults = {
+            zoomFactor: 0.05, // zoom factor per zoom tick
+            zoomDelay: 45, // how many ms between zoom ticks
+            minZoom: 0.1, // min zoom level
+            maxZoom: 10, // max zoom level
+            fitPadding: 50, // padding when fitting
+            panSpeed: 10, // how many ms in between pan ticks
+            panDistance: 10, // max pan distance per tick
+            panDragAreaSize: 75, // the length of the pan drag box in which the vector for panning is calculated (bigger = finer control of pan speed and direction)
+            panMinPercentSpeed: 0.25, // the slowest speed we can pan by (as a percent of panSpeed)
+            panInactiveArea: 8, // radius of inactive area in pan drag box
+            panIndicatorMinOpacity: 0.5, // min opacity of pan indicator (the draggable nib); scales from this to 1.0
+            zoomOnly: false, // a minimal version of the ui only with zooming (useful on systems with bad mousewheel resolution)
+            fitSelector: undefined, // selector of elements to fit
+            animateOnFit: function () { // whether to animate on fit
+                return true;
+            },
+            fitAnimationDuration: 1000, // duration of animation on fit
+
+            // icon class names
+            sliderHandleIcon: 'fa fa-minus',
+            zoomInIcon: 'fa fa-plus',
+            zoomOutIcon: 'fa fa-minus',
+            resetIcon: 'fa fa-expand'
+        };
+
+        cy.panzoom(defaults);
+
+        cy.layout({
+            name: 'cola',
+            animate: true, // whether to show the layout as it's running
+            refresh: 1, // number of ticks per frame; higher is faster but more jerky
+            maxSimulationTime: 8000, // max length in ms to run the layout
+            ungrabifyWhileSimulating: false, // so you can't drag nodes during layout
+            fit: false, // on every layout reposition of nodes, fit the viewport
+            padding: 30, // padding around the simulation
+            boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+
+            // layout event callbacks
+            ready: function () {
+            }, // on layoutready
+            stop: function () {
+            }, // on layoutstop
+
+            // positioning options
+            randomize: true, // use random node positions at beginning of layout
+            avoidOverlap: true, // if true, prevents overlap of node bounding boxes
+            handleDisconnected: true, // if true, avoids disconnected components from overlapping
+            nodeSpacing: function (node) {
+                return 10;
+            }, // extra spacing around nodes
+            flow: undefined, // use DAG/tree flow layout if specified, e.g. { axis: 'y', minSeparation: 30 }
+            alignment: undefined, // relative alignment constraints on nodes, e.g. function( node ){ return { x: 0, y: 1 } }
+
+            // different methods of specifying edge length
+            // each can be a constant numerical value or a function like `function( edge ){ return 2; }`
+            edgeLength: undefined, // sets edge length directly in simulation
+            edgeSymDiffLength: undefined, // symmetric diff edge length in simulation
+            edgeJaccardLength: undefined, // jaccard edge length in simulation
+
+            // iterations of cola algorithm; uses default values on undefined
+            unconstrIter: undefined, // unconstrained initial layout iterations
+            userConstIter: undefined, // initial layout iterations with user-specified constraints
+            allConstIter: undefined, // initial layout iterations with all constraints including non-overlap
+
+            // infinite layout options
+            infinite: true // overrides all other options for a forces-all-the-time mode
         });
+
+        cy.ready(function () {
+            $('#diagram').cytoscapeNavigator({
+                viewLiveFramerate: 0,
+                thumbnailEventFramerate: 60,
+                thumbnailLiveFramerate: true,
+                dblClickDelay: 200
+            });
+        });
+
     });
