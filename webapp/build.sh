64064c35eeada0f9a761b68d4f78365aea173ee8
Sven <sk@qyotta.de>
2016-07-10 20:39:24 +0200

    create new gh-pages branch in build script

--- webapp/build.sh
+++ webapp/build.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
-npm run build
+git branch -d gh-pages
+git push origin --delete gh-pages
+
+git checkout --orphan gh-pages
+
+npm install && npm run build
 
 cp -r dist ..
 cp index.html ..
+
+cd ..
+
+git rm -f --cached -r .
+
+git add index.html
+git add -f dist
+git add README.md
+git add LICENSE.md
+
+git commit -am 'init gh-pages'
+git push -u origin gh-pages
+
+
+git checkout -f master
